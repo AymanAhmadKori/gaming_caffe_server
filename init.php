43633d59da1699709690b5e687270f379b7c5795
AymanAhmadKori <aymanahma033@gmail.com>
2025-04-02 14:01:59 +0200

    Delete init.php

--- init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-// Define JWT keys
-define("PRIVATE_KEY", file_get_contents("includes/JWT-keys/private.key"));
-define("PUBLIC_KEY", file_get_contents("includes/JWT-keys/public.key"));
-
-// Paths
-define("jwtFile", "includes/jwt.php");
