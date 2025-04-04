ebcb4d6215bac8a3221674ca4bdb2c6357335b28
AymanAhmadKori <aymanahma033@gmail.com>
2025-04-04 23:42:13 +0200

    Update manag-accounts.php

--- APIs/manag-accounts.php
+++ APIs/manag-accounts.php
@@ -11,13 +11,19 @@ include 'init.php';
   - - **`by` T string [ email - id ]
   - - ** `email` T string || ** `id` T int
 
-  - ban-account & unBlock-account : returns bool
-  - - ** `id` T int
+  - ban-account : returns bool
+  - - ** `account_id` T int
+  - - ** `ban_type` T int
+  - - ** `Unblock_at` T string [ISO time]
+
+  - unBlock-account : returns bool
+  - - ** `account_id` T int
 
   - set-sub : returns bool
   - - ** `account-id` T int
   - - ** `plan-id` T int
-  - - ** `plan-cycles` T int
+  - - ** `cost` T float
+  - - ** `expiry` T string [ISO time]
 
   - get-sub: returns object || null
   - - ** `account-id` T int
@@ -66,8 +72,14 @@ function error(string $type) {
       //
 
       // ban-account & unBlock-account
-        case '!isset: id in ban-account || unBlock-account mode':
-          $message('Undefined `id`: id is require in `ban-account` or `unBlock-account mode');
+        case '!isset: account_id in ban-account || unBlock-account mode':
+          $message('Undefined `account_id`: account_id is require in `ban-account` or `unBlock-account mode');
+          break;
+        case '!isset: ban_type in ban-account mode':
+          $message('Undefined `ban_type`: ban_type is require in ban-account mode');
+          break;
+        case '!isset: unBlock_at in ban-account mode':
+          $message('Undefined `unBlock_at`: unBlock_at is require in ban-account mode');
         break;
       //
 
@@ -78,8 +90,11 @@ function error(string $type) {
         case '!isset: plan_id in set-sub mode':
           $message('Undefined `plan_id` : plan_id is require in `set-sub` mode');
           break;
-        case '!isset: plan_cycles in set-sub mode':
-          $message('Undefined `plan_cycles` : plan_cycles is require in `set-sub` mode');
+        case '!isset: expiry in set-sub mode':
+          $message('Undefined `expiry` : expiry is require in `set-sub` mode');
+          break;
+        case '!isset: cost in set-sub mode':
+          $message('Undefined `cost` : cost is require in `set-sub` mode');
         break;
       //
       
@@ -131,8 +146,14 @@ function error(string $type) {
       //
 
       // ban-account || unblock-account mode
-        case 'invalid: id in ban-account || unblock-account mode':
-          $message('Invalid `id` value');
+        case 'invalid: account_id in ban-account || unblock-account mode':
+          $message('Invalid `account_id` value');
+          break;
+        case 'invalid: ban_type in ban-account mode':
+          $message('Invalid `ban_type` value');
+          break;
+        case 'invalid: unBlock_at in ban-account mode':
+          $message('Invalid `unBlock_at` value not-ISO8601');
         break;
       //
 
@@ -143,8 +164,11 @@ function error(string $type) {
         case 'invalid: plan_id in set-sub mode':
           $message('Invalid `plan_id` value');
           break;
-        case 'invalid: plan_cycles in set-sub mode':
-          $message('Invalid `plan_cycles` value');
+        case 'invalid: expiry in set-sub mode':
+          $message('Invalid `expiry` value not-ISO8601');
+          break;
+        case 'invalid: cost in set-sub mode':
+          $message('Invalid `cost` value');
         break;
       //
       
@@ -233,7 +257,7 @@ define('search_account_modes',[
       
         // Check except IDs
         $types = array_unique(array_map('gettype', $except));
-        if(count($types) !== 1 || $types[0] !== 'integer') error('invalid: except value');
+        if( count($types) !== 0 && (count($types) !== 1 || $types[0] !== 'integer') ) error('invalid: except value');
       //
     break;
 
@@ -268,10 +292,27 @@ define('search_account_modes',[
     case 'ban-account':
     case 'unBlock-account':
       // Validate `id`
-      if(!isset($data['id'])) error('!isset: id in ban-account || unBlock-account mode');
-      $id = $data['id'];
+      if(!isset($data['account_id'])) error('!isset: account_id in ban-account || unBlock-account mode');
+      $account_id = $data['account_id'];
       
-      if( !is_int($id) || $id < 0) error('invalid: id in ban-account || unblock-account mode');
+      if( !is_int($account_id) || $account_id < 0) error('invalid: account_id in ban-account || unblock-account mode');
+
+      // Stop validating if on unBlock-account mode
+      if($mode == 'unBlock-account') return;
+
+      // Validate `ban_type`
+      if( !isset($data['ban_type']) ) error('!isset: ban_type in ban-account mode');
+        $ban_type = $data['ban_type'];
+
+        if( !is_int($ban_type) || $ban_type < 0) error('invalid: ban_type in ban-account mode');
+      //
+
+      // Validate `unBlock_at`
+      if( !isset($data['unBlock_at']) ) error('!isset: unBlock_at in ban-account mode');
+        $unBlock_at = $data['unBlock_at'];
+
+        if(empty($unBlock_at) || !isValidISO8601($unBlock_at)) error('invalid: unBlock_at in ban-account mode');
+      //
     break;
 
     case 'set-sub':
@@ -288,14 +329,20 @@ define('search_account_modes',[
 
         if( !is_int($plan_id) || $plan_id < 0 ) error('invalid: plan_id in set-sub mode');
       //
-
-      // Validate plan_cycles
-      if( !isset($data['plan_cycles']) ) error('!isset: plan_cycles in set-sub mode');
-        $plan_cycles = $data['plan_cycles'];
-
-        if( !is_int($plan_cycles) || $plan_cycles < 0) error('invalid: plan_cycles in set-sub mode');
-      //
       
+      // Validate expiry
+      if( !isset($data['expiry']) ) error('!isset: expiry in set-sub mode');
+        $expiry = $data['expiry'];
+        
+        if( !isValidISO8601($expiry) ) error('invalid: expiry in set-sub mode');
+      //
+
+      // Validate cost
+      if( !isset($data['cost']) ) error('!isset: cost in set-sub mode');
+        $cost = $data['cost'];
+        
+        if(empty($cost) || !in_array(gettype($cost), ['integer', 'double']) || $cost < 0) error('invalid: cost in set-sub mode');
+      //
     break;
 
     case 'get-sub':
@@ -340,3 +387,134 @@ define('search_account_modes',[
   }
   
 })();
+
+// Get request mode
+$request_mode = $data['mode'];
+
+// Response with
+function response_with($data) {
+  // Show response
+  echo json_encode([
+    "data" => $data
+  ]);
+
+  // Exit;
+  exit();
+}
+
+switch ($request_mode) {
+  // Returns: Array of objects [{id, full_name, email}]
+  case 'getAll-accounts':
+    // Limit & Excepts
+    $limit = $data['limit'];
+    $except = array_unique($data['except']);
+    
+    // Query
+    $q = "SELECT `id`, `full_name`, `email`, `entry_date` FROM `accounts` ";
+    $params = [...$except];
+
+    // Add except in query
+    if( count($except) > 0 ) {
+      $q .= 'WHERE `id` NOT IN('. implode(',', array_map(fn()=> '?', $except)) .') '; // (?,?,?)
+    }
+
+    // Add LIMIT in query
+    $q .= "LIMIT " . $limit;
+    
+    // Connect to database
+    include connectFile;
+
+    // Create statment & executed
+    $stmt = $pdo->prepare($q);
+    $stmt->execute($params);
+
+    // Get response from statment
+    $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    // Close database connection
+    unset($pdo);
+    
+    // Response data
+    response_with($response);
+  break;
+
+  // Return: Object {id, full_name, email, ban}
+  case 'search-account': break;
+
+  // Returns: bool [true | false]
+  case 'ban-account':
+    // Get account_id & ban_type & unBlock_at
+    $account_id = $data['account_id'];
+    $ban_type = $data['ban_type'];
+    $unBlock_at = $data['unBlock_at'];
+
+    // Create query
+    $q = 'INSERT INTO `ban_details`(`account_id`, `ban_type`, `Unblock_at`) VALUES (?,?,?)';
+
+    // Connect to database
+    include connectFile;
+
+    // Execute statment
+    $stmt = execute_statment($q, [$account_id, $ban_type, $unBlock_at]);
+    
+    // Response data
+    if( is_null($stmt) ) response_with(false);
+    else response_with(true);
+  break;
+
+  // Returns: bool [true | false]
+  case 'unBlock-account':
+    // Get account id
+    $account_id = $data['account_id'];
+
+    // Connect to database
+    include connectFile;
+    
+    // Create statment and executed
+    $stmt = $pdo->prepare('DELETE FROM `ban_details` WHERE `account_id` = ?');
+    $stmt->execute([$account_id]);
+
+    // Response data
+    response_with($stmt->rowCount() > 0);
+  break;
+
+  // Returns: bool [true | false]
+  case 'set-sub':
+    // Get data
+    $account_id = $data['account_id'];
+    $plan_id = $data['plan_id'];
+    $cost = $data['cost'];
+    $expiry = $data['expiry'];
+
+    // Create query
+    $q = "INSERT INTO `subs`(`account_id`, `plan_id`, `cost`, `expiry`) VALUES (?,?,?,?)";
+
+    // Conncet to database
+    include connectFile;
+    
+    // Execute statment
+    $stmt = execute_statment($q, [$account_id, $plan_id, $cost, $expiry]);
+
+    // Response data
+    if( is_null($stmt) ) response_with(false);
+    else response_with(true);
+  break;
+
+  // Returns: bool [true | false]
+  case 'cancel-sub':
+    $account_id = $data['account_id'];
+
+    // Query
+    $q = "DELETE FROM `subs` WHERE `account_id` = ?";
+
+    // Conncet to database
+    include connectFile;
+
+    // Execute statment
+    $stmt = execute_statment($q, [$account_id]);
+
+    // Response data
+    if(is_null($stmt) || $stmt->rowCount() == 0) response_with(false);
+    else response_with(true);
+  break;
+}
